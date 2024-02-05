a8cc38632bdcefec14a5373d8b0b8cc6e2bb562c
Sammo <c2031078@exchange.shu.ac.uk>
2024-02-05 13:39:53 +0000

    Moved Custom Marker To Components

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
-import { Marker } from "react-leaflet";
-import { Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../css/leaflet.css";
-
-const center = [52.998463846073854, -1.2682985457466047];
+import Marker from "./CustomMarker";
 
 const Map = () => {
   return (
     <div className="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={[54.091617, -1.793925]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           accessToken={process.env.REACT_APP_API_KEY}
           url="https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker
+          Latitude={54.091617}
+          Longitude={-1.793925}
+          PopupContent={
+            <h1>
+              POPUP! <strong>Hello</strong>
+            </h1>
+          }
+        />
       </MapContainer>
     </div>
   );
