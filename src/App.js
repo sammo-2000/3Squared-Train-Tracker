d8c65ac2212716590cf193ca4bf4881cbfc79dc7
emorrisn <bf4spartan@gmail.com>
2024-02-05 17:52:34 +0000

    progress?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Search />
-      {/* <Map /> */}
+      <Map />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,16 @@
 import { Command } from 'cmdk';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import "../css/search.scss";
+import * as Popover from '@radix-ui/react-popover'
 
 // Use Command/CTRL + K to activate
 
-
 const Search = () => {
-  const [open, setOpen] = React.useState(false)
-  const [loading, setLoading] = React.useState(false)
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('linear');
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const listRef = useRef(null)
 
   useEffect(() => {
     const down = (e) => {
@@ -21,26 +24,34 @@ const Search = () => {
     return () => document.removeEventListener('keydown', down)
   }, []);
 
+  const style = {
+    position: 'absolute',
+    width: '100%',
+    zIndex: 1000,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'white',
+  };
+
   return (
-    <div className="raycast">
-      <Command.Dialog open={open} onOpenChange={setOpen}>
-        <Command.Input />
-
-        <Command.List>
-          {loading && <Command.Loading>Loading…</Command.Loading>}
-
-          <Command.Empty>No results found!</Command.Empty>
-
-          <Command.Group heading="Fruits">
-            <Command.Item>Apple</Command.Item>
-            <Command.Item>Orange</Command.Item>
-            <Command.Separator />
-            <Command.Item>Pear</Command.Item>
-            <Command.Item>Blueberry</Command.Item>
+    <div style={style}>
+      <Command.Dialog className="raycast" open={open} onOpenChange={setOpen} style={style}>
+      <Command loop>
+      <Command.Input id={inputRef} autoFocus placeholder="Search for trains and stations..." />
+        <hr cmdk-raycast-loader="" />
+        <Command.List id={listRef}>
+          <Command.Empty>No results found.</Command.Empty>
+          <Command.Group heading="Stations">
+            <Command.Item onSelect={(value) => console.log('Selected', value)}>Station 123847ABQWZ</Command.Item>
+            <Command.Item onSelect={(value) => console.log('Selected', value)}>Station 87987HJUGA</Command.Item>
           </Command.Group>
-
-          <Command.Item>Fish</Command.Item>
+          <Command.Group heading="Trains">
+            <Command.Item onSelect={(value) => console.log('Selected', value)}>Train 239847KJUGHdsfui</Command.Item>
+            <Command.Item onSelect={(value) => console.log('Selected', value)}>Train sdkik2893849798</Command.Item>
+            <Command.Item onSelect={(value) => console.log('Selected', value)}>Train 123444789KJHJ</Command.Item>
+          </Command.Group>
         </Command.List>
+        </Command>
       </Command.Dialog>
     </div>
   );
