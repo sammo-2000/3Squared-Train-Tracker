def33f49fc6adc41a86b2f8d4224193eb1716023
Matthew Richards <116161023+mRichardio@users.noreply.github.com>
2024-02-10 00:42:19 +0000

    style: added box shadow to the navbar & 3squared logo
    Still just testing out ideas (also not sure why the background is transparent?)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='absolute top-[5.5rem] left-0 w-1/12 h-1/1 bg-gray-400 flex-col text-center z-[1000] m-3 rounded-lg'>
-    {/* nav content */}
-    <img src="#" alt="3Squared Logo" />
-    <strong><h1>Navigation</h1></strong>
-    <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
-    <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
-    <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
-    <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
-  </div>
+    <div className="shadow-box">
+      <div className='absolute top-[5.5rem] left-0 w-1/12 h-1/1 flex-col text-center z-[1000] m-3 rounded-lg overflow-hidden shadow-box' style={{
+        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset'
+      }}>
+        {/* nav content */}
+        <img src="https://3squared.com/wp-content/webp-express/webp-images/uploads/2023/10/3Squared-Logo-white.png.webp" alt="3Squared Logo" />
+        <strong><h1>Navigation</h1></strong>
+        <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
+        <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
+        <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
+        <h3 className='m-1 hover:bg-white rounded-lg'>Test</h3>
+      </div>
+    </div>
   )
 }
 
