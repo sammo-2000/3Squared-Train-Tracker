d1f7716c0d38167070afe8e7c45e3ae18cadc54f
sammo <ayman.sammo75@gmail.com>
2024-02-12 13:01:19 +0000

    Installed React, Tailwind & Ant Design

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Component
+import Map from "./components/Map";
+
+const App = () => {
+  return (
+    <>
+      <Map />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import "leaflet/dist/leaflet.css";
+import "../css/leaflet.css";
+
+const Map = () => {
+  return (
+    <div className="map">
+      <MapContainer
+        center={[54.091617, -1.793925]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          accessToken={process.env.REACT_APP_MAP_API_KEY}
+          url="https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token={accessToken}"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
