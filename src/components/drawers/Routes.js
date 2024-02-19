778929ab3ee6fbbd5b938b57ab6bc1761fd6a64b
musicjoe12 <josephgrierson8@gmail.com>
2024-02-19 13:02:44 +0000

    Route & train design fix

--- src/components/drawers/Routes.js
+++ src/components/drawers/Routes.js
@@ -1,18 +1,20 @@
-import { Drawer, Space, Button } from "antd";
+import { Drawer, Space, Button, Empty } from "antd";
 import "../../css/drawer.css";
 import { useState } from "react";
+
 import add from "../../assets/add.svg";
 import back from "../../assets/back.svg";
+import mapEmpty from "../../assets/map-empty.svg";
 
 const Routes = (props) => {
-  const [overlayVisible, setOverlayVisible] = useState(false);
+  const [childrenDrawer, setChildrenDrawer] = useState(false);
 
-  const showOverlay = () => {
-    setOverlayVisible(true);
+  const showChildrenDrawer = () => {
+    setChildrenDrawer(true);
   };
 
-  const hideOverlay = () => {
-    setOverlayVisible(false);
+  const onChildrenDrawerClose = () => {
+    setChildrenDrawer(false);
   };
 
   return (
@@ -22,35 +24,58 @@ const Routes = (props) => {
         onClose={() => {
           props.setActiveDraw("menu");
         }}
-        visible={true}
+        open={true}
         placement="left"
         closeIcon={<img src={back} />}
         extra={
           <Space>
-          <Button
-            onClick={null}
-            shape="circle"
-            type="primary"
-            ghost
-            icon={<img src={add} />}
-          ></Button>
-        </Space>
+            <Button
+              onClick={showChildrenDrawer}
+              shape="circle"
+              type="primary"
+              ghost
+              icon={
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1.5rem"
+                    height="1.5rem"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    viewBox="0 -960 960 960"
+                  >
+                    <path d="M450.001-450.001h-200q-12.75 0-21.375-8.628-8.625-8.629-8.625-21.384 0-12.756 8.625-21.371 8.625-8.615 21.375-8.615h200v-200q0-12.75 8.628-21.375 8.629-8.625 21.384-8.625 12.756 0 21.371 8.625 8.615 8.625 8.615 21.375v200h200q12.75 0 21.375 8.628 8.625 8.629 8.625 21.384 0 12.756-8.625 21.371-8.625 8.615-21.375 8.615h-200v200q0 12.75-8.628 21.375-8.629 8.625-21.384 8.625-12.756 0-21.371-8.625-8.615-8.625-8.615-21.375v-200Z" />
+                  </svg>
+                </>
+              }
+            ></Button>
+          </Space>
         }
       >
-        
-      </Drawer>
+        <Empty
+          image={mapEmpty}
+          imageStyle={{
+            height: 60,
+            alignSelf: "center",
+            display: "inline-block",
+          }}
+          description={
+            <span className="font-semibold">
+              You have no Routes tracked.
+            </span>
+          }
+        ></Empty>
 
-      
-      <Drawer
-        title="Routes"
-        placement="left"
-        closable={true}
-        onClose={hideOverlay}
-        visible={overlayVisible}
-        width={400}
-      >
-        <h1>Trains</h1>
-        
+        <Drawer
+          title="Two-level Drawer"
+          width={320}
+          closable={false}
+          onClose={onChildrenDrawerClose}
+          open={childrenDrawer}
+          placement="left"
+        >
+          This is two-level drawer
+        </Drawer>
       </Drawer>
     </>
   );
--- src/components/drawers/Trains.js
+++ src/components/drawers/Trains.js
@@ -1,35 +1,84 @@
-import { Drawer, Space, Button } from "antd";
+import { Drawer, Space, Button, Empty } from "antd";
 import "../../css/drawer.css";
 import { useState } from "react";
+
 import add from "../../assets/add.svg";
 import back from "../../assets/back.svg";
+import mapEmpty from "../../assets/map-empty.svg";
 
 const Trains = (props) => {
-    return (
-      <>
-        <Drawer
-          title="Trains"
-          onClose={() => {
-            props.setActiveDraw("menu");
-          }}
-          open={true}
-          placement="left"
-          closeIcon={<img src={back} />}
-          extra={
-            <Space>
+  const [childrenDrawer, setChildrenDrawer] = useState(false);
+
+  const showChildrenDrawer = () => {
+    setChildrenDrawer(true);
+  };
+
+  const onChildrenDrawerClose = () => {
+    setChildrenDrawer(false);
+  };
+
+  return (
+    <>
+      <Drawer
+        title="Tracked Trains"
+        onClose={() => {
+          props.setActiveDraw("menu");
+        }}
+        open={true}
+        placement="left"
+        closeIcon={<img src={back} />}
+        extra={
+          <Space>
             <Button
-              onClick={null}
+              onClick={showChildrenDrawer}
               shape="circle"
               type="primary"
               ghost
-              icon={<img src={add} />}
+              icon={
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1.5rem"
+                    height="1.5rem"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    viewBox="0 -960 960 960"
+                  >
+                    <path d="M450.001-450.001h-200q-12.75 0-21.375-8.628-8.625-8.629-8.625-21.384 0-12.756 8.625-21.371 8.625-8.615 21.375-8.615h200v-200q0-12.75 8.628-21.375 8.629-8.625 21.384-8.625 12.756 0 21.371 8.625 8.615 8.625 8.615 21.375v200h200q12.75 0 21.375 8.628 8.625 8.629 8.625 21.384 0 12.756-8.625 21.371-8.625 8.615-21.375 8.615h-200v200q0 12.75-8.628 21.375-8.629 8.625-21.384 8.625-12.756 0-21.371-8.625-8.615-8.625-8.615-21.375v-200Z" />
+                  </svg>
+                </>
+              }
             ></Button>
           </Space>
         }
+      >
+        <Empty
+          image={mapEmpty}
+          imageStyle={{
+            height: 60,
+            alignSelf: "center",
+            display: "inline-block",
+          }}
+          description={
+            <span className="font-semibold">
+              You have no Trains tracked.
+            </span>
+          }
+        ></Empty>
+
+        <Drawer
+          title="Two-level Drawer"
+          width={320}
+          closable={false}
+          onClose={onChildrenDrawerClose}
+          open={childrenDrawer}
+          placement="left"
         >
+          This is two-level drawer
         </Drawer>
-      </>
-    );
-  };
+      </Drawer>
+    </>
+  );
+};
 
 export default Trains;
