6290ea06c1a2ee09e9e3046cc7cda6237eb7d36a
musicjoe12 <josephgrierson8@gmail.com>
2024-02-19 12:18:37 +0000

    Routes

--- src/components/drawers/Routes.js
+++ src/components/drawers/Routes.js
@@ -1,23 +1,49 @@
-import { Drawer } from "antd";
+import { Drawer, Button, Input, Space } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import "../../css/drawer.css";
 import { useState } from "react";
 
 const Routes = (props) => {
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+  const showOverlay = () => {
+    setOverlayVisible(true);
+  };
+
+  const hideOverlay = () => {
+    setOverlayVisible(false);
+  };
+
   return (
     <>
       <Drawer
-        title="Basic Drawer"
+        title="Tracked Routes"
         onClose={() => {
           props.setActiveDraw("menu");
         }}
-        open={true}
+        visible={true}
         placement="left"
       >
         <h1>Tracked Routes</h1>
+        <div className="overlayOpen"><Space style={{ marginTop: '20px' }}>
+          <Button type="primary" onClick={showOverlay} icon={<PlusOutlined />} />
+        </Space>
+        </div>
+        <Input.Search placeholder="Search" />
+        
+      </Drawer>
 
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      //Add Trains overlay
+      <Drawer
+        title="Routes"
+        placement="left"
+        closable={true}
+        onClose={hideOverlay}
+        visible={overlayVisible}
+        width={400}
+      >
+        <h1>Trains</h1>
+        
       </Drawer>
     </>
   );
