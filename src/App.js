34b894a86bcf6dde814839b429ff295e43efea1d
sammo <ayman.sammo75@gmail.com>
2024-02-19 21:59:02 +0000

    tiploc API wrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // Component
 import Map from "./components/Map";
 
+// Context
+import { UseSelectedTiploc } from "./hooks/SelectedTiplocHook.js";
+
 const App = () => {
-  return <Map />;
+  const { setSelectedTiploc } = UseSelectedTiploc();
+
+  useEffect(() => {
+    setSelectedTiploc([{ id: "123", title: "LEEDS", tiploc: "LEEDS" }]);
+  }, [setSelectedTiploc]);
+
+  return (
+    <>
+      <Map />
+    </>
+  );
 };
 
 export default App;
