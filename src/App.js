1969ffbceab42c9eeb8bfc3534a7c547e55a7884
sammo <ayman.sammo75@gmail.com>
2024-02-19 11:22:53 +0000

    Added loading animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 // API
 import { tiplocAPI } from "./api/data.js";
@@ -7,19 +8,24 @@ import { tiplocAPI } from "./api/data.js";
 import Map from "./components/Map";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getData = async () => {
       const data = await tiplocAPI(["LEEDS"], "2024-02-18", "2024-02-20");
       console.log(data);
+      setLoading(false);
     };
 
     getData();
   }, []);
 
-  return (
-    <>
-      <Map />
-    </>
+  return loading ? (
+    <div className="h-screen w-full flex justify-center items-center">
+      <HashLoader color="#36d7b7" size={50} />
+    </div>
+  ) : (
+    <Map />
   );
 };
 
