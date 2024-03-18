d8f99995e1888361dfd5db278d1059b3e06836a2
mRichardio <c2019629@hallam.shu.ac.uk>
2024-03-18 12:45:04 +0000

    feat: Tracker Component core functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Draggable from 'react-draggable'; // Import Draggable
+import Draggable from "react-draggable"; // Import Draggable
 
 import logo from "../assets/icons/3squared.jpg";
 import Icon from "./Icons";
@@ -7,6 +7,7 @@ import Icon from "./Icons";
 import Locations from "./drawers/Locations";
 import Trains from "./drawers/Trains";
 import Routes from "./drawers/Routes";
+import Tracker from "./drawers/Tracker";
 
 import Settings from "./modals/Settings";
 import { useSettings } from "../hooks/SettingsHook";
@@ -39,26 +40,36 @@ function Navbar() {
         isOpen={activeDrawer === "routes"}
         setActiveDraw={setActiveDrawer}
       />
+
+      <Tracker
+        isOpen={activeDrawer === "tracker"}
+        setActiveDraw={setActiveDrawer}
+      />
+
       <Settings isOpen={settingsModal} setOpen={setSettingsModal} />
-      
-        <div className="">
-          <Draggable bounds="body" grid={[1, 1]} >
+
+      <div className="">
+        <Draggable bounds="body" grid={[1, 1]}>
           <div
             className={`absolute top-0 ${
               settings.menuDirection.value === "right" ? "right-0" : "left-0"
             } flex flex-col text-center z-[1000] m-3 rounded-xl bg-white border-2 border-gray-100 overflow-hidden divide-x-2 divide-y-2 divide-gray-100`}
-            >
+          >
             <div
               key={0}
               className="flex items-center flex-col transition-color duration-200 justify-center p-4 text-gray-700"
-              >
-              <img style={{ width: "7rem", pointerEvents: 'none' }} src={logo} alt={`Icon ${0}`}/>
+            >
+              <img
+                style={{ width: "7rem", pointerEvents: "none" }}
+                src={logo}
+                alt={`Icon ${0}`}
+              />
             </div>
             <div
               key={1}
               className="flex items-center flex-col transition-color duration-200 hover:text-blue-600 hover:bg-blue-100 justify-center p-4 cursor-pointer text-gray-700"
               onClick={() => setActiveDrawer("locations")}
-              >
+            >
               <Icon iconName="location" />
               <span>Locations</span>
             </div>
@@ -73,14 +84,22 @@ function Navbar() {
             <div
               key={4}
               className="flex items-center flex-col transition-color duration-200 hover:text-blue-600 hover:bg-blue-100 justify-center p-4 cursor-pointer"
+              onClick={() => setActiveDrawer("tracker")}
+            >
+              <Icon iconName="route" />
+              <span>Route Tracker</span>
+            </div>
+            <div
+              key={5}
+              className="flex items-center flex-col transition-color duration-200 hover:text-blue-600 hover:bg-blue-100 justify-center p-4 cursor-pointer"
               onClick={() => setSettingsModal(settingsModal ? false : true)}
-              >
+            >
               <Icon iconName="settings" />
               <span>Settings</span>
             </div>
           </div>
-          </Draggable>
-        </div>
+        </Draggable>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        150: "150%", // Add this line
+      },
+    },
   },
   plugins: [],
 };
