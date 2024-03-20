eb53fd8b132249dd659ee591ec55758033d6361c
mRichardio <c2019629@hallam.shu.ac.uk>
2024-03-20 13:27:57 +0000

    tour: final commit, feature done

--- client/src/components/Guide.js
+++ client/src/components/Guide.js
@@ -3,32 +3,47 @@ import { EllipsisOutlined } from "@ant-design/icons";
 import { Button, Divider, Space, Tour } from "antd";
 
 import tourImage1 from "../assets/images/TourImage1.png";
-import tourImage2 from "../assets/images/TourImage1.png";
-import tourImage3 from "../assets/images/TourImage1.png";
+import tourImage2 from "../assets/images/TourImage2.png";
+import tourImage3 from "../assets/images/TourImage3.png";
 import tourGif from "../assets/gifs/TourGif4.gif";
 
 const Guide = ({ open, setOpen, refs }) => {
   console.log(refs);
   const steps = [
     {
-      title: "Upload File",
-      description: "Put your files here.",
+      title: "Locations",
+      description:
+        "This section will allow you to view and track all of the available TIPLOC Locations on the map. Tracking a location will then allow you to view all available routes to and from that location in the routes section.",
       cover: (
         <img
-          alt="tour.png"
-          src="https://user-images.githubusercontent.com/5378891/197385811-55df8480-7ff4-44bd-9d43-a7dade598d70.png"
+          alt="Image giving a brief overview of the locations section"
+          src={tourImage1}
         />
       ),
       target: () => refs[0].current,
     },
     {
-      title: "Save",
-      description: "Save your changes.",
+      title: "Routes",
+      description:
+        "The routes section will allow you to view and track all of the available routes based on your tracked locations. Tracking a route will then plot the route and it's schedule on he map. This section also provides a journey tracker for your to be able to view the progress of the train.",
+      cover: (
+        <img
+          alt="Image giving a brief overview of the routes section"
+          src={tourImage2}
+        />
+      ),
       target: () => refs[1].current,
     },
     {
-      title: "Other Actions",
-      description: "Click to see other actions.",
+      title: "Settings",
+      description:
+        "This is the settings section, here you can change the way the application looks and behaves to fit your own personal preferences.",
+      cover: (
+        <img
+          alt="Image giving a brief overview of the settings section"
+          src={tourImage3}
+        />
+      ),
       target: () => refs[2].current,
     },
     {
